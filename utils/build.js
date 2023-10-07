e24ae2bb6c78376377dd311857220bd6e5f7d8c0
tree <tree@hidden.cz>
2023-10-08 01:57:19 +0200

    update deploy

--- utils/build.js
+++ utils/build.js
@@ -1,3 +1,4 @@
+import { emptyDir } from "https://deno.land/std@0.203.0/fs/empty_dir.ts";
 import { W3PData } from "./w3pdata.js";
 
 const w3pd = new W3PData();
@@ -6,9 +7,9 @@ await w3pd.init();
 const outputDir = "./dist";
 
 try {
-  await Deno.mkdir(outputDir);
+  await emptyDir(outputDir);
 } catch {}
 
-const bundleFn = `${outputDir}/bundle.json`;
+const bundleFn = `${outputDir}/index.json`;
 await Deno.writeTextFile(bundleFn, JSON.stringify(w3pd.data, null, 2));
 console.log(`Bundle writed: ${bundleFn}`);
